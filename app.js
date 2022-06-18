78fadcd4bf2313794174d73560ae61379cecf1c3
it'sme <94110881+salmadewinh@users.noreply.github.com>
2022-06-18 23:44:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.get('/', function (req, res) {
 	res.json({
 	  type: 'get',
 	  number: 1,
-	  nama: 'Okyza Maherdy Prabowo',
-	  npm: '33220025'
+	  nama: 'Salma Dewi Nurhabibah',
+	  npm: '2042018'
 	});
   })
 
